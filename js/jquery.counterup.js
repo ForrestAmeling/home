7f892c7abf52ea0842019651d6c0a740b33673e3
GumpGaming <anxin4392@gmail.com>
2023-06-13 16:19:11 -0400

    More NAV changes!

--- js/jquery.counterup.js
+++ js/jquery.counterup.js
@@ -83,8 +83,12 @@
     $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
             $('nav.navbar').addClass('navbar-shrink');
+            $('nav.navbar .navbar-header h3').css('margin-top', '8px');
+            $('nav.navbar .navbar-default .navbar-nav li a').css('padding', '8px 15px');
         } else {
             $('nav.navbar').removeClass('navbar-shrink');
+            $('nav.navbar .navbar-header h3').css('margin-top', '15px');
+            $('nav.navbar .navbar-default .navbar-nav li a').css('padding', '10px 26px');
         }
     });
 
