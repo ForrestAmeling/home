e9dbc1c4f8a18608a1508b9567ba3ac27d3eb34e
GumpGaming <anxin4392@gmail.com>
2023-06-14 11:30:53 -0400

    python project

--- js/main.js
+++ js/main.js
@@ -11,6 +11,23 @@ $(document).ready(function () {
 		time: 1000
 	});
 
+	// Replace with your GitHub username, repository, and file path
+	const username = "forrestameling";
+	const repository = "BesbuyGpuBot";
+	const filePath = "blob/main/bestbuy.py";
+
+	// Fetch the file content from GitHub API
+	fetch(`https://api.github.com/repos/${username}/${repository}/contents/${filePath}`)
+		.then(response => response.json())
+		.then(data => {
+			const fileContent = atob(data.content); // Decode base64 content
+
+			// Display the file content on the page
+			const fileContentElement = document.getElementById("file-content");
+			fileContentElement.innerText = fileContent;
+		})
+		.catch(error => console.error(error));
+
 
 });
 
